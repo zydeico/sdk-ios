04ba6a358ef2c60019268aac47f4333d9932fffb
Guilherme Prata Costa <guilherme.pcosta@mercadolivre.com>
2024-12-23 15:17:47 -0300

    [2321](Feature) Added SwiftLint (#4)
    * Initial swift lint
    
    * package resolved

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "MercadoPagoSDK-iOS",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -12,11 +15,18 @@ let package = Package(
             targets: ["CoreMethods"]
         )
     ],
+    dependencies: [
+        // SwiftLint plugin dependency
+        .package(url: "https://github.com/SimplyDanny/SwiftLintPlugins", from: "0.57.1")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CoreMethods"
+            name: "CoreMethods",
+            plugins: [
+                .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLintPlugins")
+            ]
         ),
         .testTarget(
             name: "CoreMethodsTests",
